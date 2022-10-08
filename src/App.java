e597eec0fcd9a4edec50a78e6fcbd4f034412200
chengivan01 <chengoleivan@gmail.com>
2022-10-08 23:01:47 +0300

    For loop learnt

--- src/App.java
+++ src/App.java
@@ -2,19 +2,11 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("How many lines do you want?");
         Scanner cases = new Scanner(System.in);
         int cases_num = cases.nextInt();
-        String day = switch(cases_num) {
-            case 1 -> "Monday";
-            case 2 -> "Tuesday";
-            case 3 -> "Wednesday";
-            case 4 -> "Thursday";
-            case 5 -> "Friday";
-            case 6 -> "Saturday";
-            case 7 -> "Sunday";
-            default -> "Invalid day number";
-        };
-
-        System.out.println(day);
+        for(int x = 1; x <= cases_num; x++) {
+            System.out.println("You wanted this many lines");
+        }
     }
 }
