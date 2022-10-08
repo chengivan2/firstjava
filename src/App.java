5b726bbd6e43f41dadcea35b3635d8d92da8a559
chengivan01 <chengoleivan@gmail.com>
2022-10-08 10:36:40 +0300

    If else statements

--- src/App.java
+++ src/App.java
@@ -6,5 +6,14 @@ public class App {
         String name = name1.nextLine();
         System.out.println("Hello, World!");
         System.out.println(name);
+        Scanner time1 = new Scanner(System.in);
+        int time = time1.nextInt();
+        if (time > 12) {
+            System.out.println("Morning");
+        }
+        
+        else if (time < 12) {
+            System.out.println("Evening");
+        }
     }
 }
