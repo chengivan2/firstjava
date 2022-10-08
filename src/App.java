3750d5395d146e033968da99f3f10aefc2c83624
chengivan01 <chengoleivan@gmail.com>
2022-10-08 03:30:16 +0300

    Java input sing Scanner class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner name1 = new Scanner(System.in);
+        String name = name1.nextLine();
         System.out.println("Hello, World!");
+        System.out.println(name);
     }
 }
