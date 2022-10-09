98a16f2958d9bb4eeb81264e500e803da81a0b2d
chengivan01 <chengoleivan@gmail.com>
2022-10-09 13:40:33 +0300

    Advanced for loop on multidimentional array explained

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         String [] [] names = new String [] [] {{"Ivan"}, {"Oyeezy"}, {"Warren"}, {"Mnyang'oo"}, {"Cheng'ole", "Enock", "Biggedi"}};
-        for (String[] element: names) {
+        for (String[]  element: names) { //The variable is set to an array since the elements are arrays
             System.out.println(element[0]);
         }
     }
